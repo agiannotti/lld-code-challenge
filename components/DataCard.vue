04833b7ad924431bd26240035e66f47fa1077eda
tonyg <aj.gianno@gmail.com>
2021-05-14 20:29:30 -0500

    api request success!

--- components/DataCard.vue
+++ components/DataCard.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="md:flex">
     <div class="md:flex-shrink-0">
-      <img class="md:w-56" :src="data.image" :alt="data.name" />
+      <!-- <img class="md:w-56" :src="data.image" :alt="data.name" /> -->
     </div>
     <div>
       <div class="text-sm font-bold">
-        {{ data.descriptione }}
+        <!-- {{ data.description }} -->
       </div>
-      <p class="mt-2 text-gray-600">{{ data.price }}</p>
+      <li v-for="data in product" :key="data.index"><br />{{ data.name }}</li>
+
+      <!-- <p class="mt-2 text-gray-600">{{ data.price }}</p> -->
     </div>
   </div>
 </template>
--- pages/data.vue
+++ pages/data.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="container mx-auto mt-8">
-    <DataCard v-for="product in products" :data="products" :key="products.id" />
+    <!-- <DataCard v-for="(product, index) in products" :key="index" /> -->
+    <ul>
+      <div v-for="(product, index) in products" :key="index" :product="product">
+        <li v-for="data in product" :key="data.index">
+          <br />{{ data.name }} {{ data.price }}{{ data.description }}
+          <img :src="data.image" :alt="data.name" />
+        </li>
+      </div>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  async fetch() {
-    this.products = await fetch(
-      'https://trayvonnorthern.com/Edgewood-API/public/api/products'
-    ).then((res) => console.log(res.json()));
+  async asyncData({ $axios }) {
+    const API_URL =
+      'https://trayvonnorthern.com/Edgewood-API/public/api/products';
+    const products = await $axios.$get(API_URL);
+    console.log(products.data[0].id);
+    return { products };
   },
-  data() {
-    return {
-      products: {},
-    };
-  },
+  data() {},
 };
 </script>
